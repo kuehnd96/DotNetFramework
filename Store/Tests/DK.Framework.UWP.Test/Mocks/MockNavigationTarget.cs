1fc5c0b1fa076f4afe89665c9213b3969d7ff761
David Kuehn <kuehnd96@gmail.com>
2015-08-26 21:43:03 -0500

    Reproduced all UWP framework tests.

---
+++ Store/Tests/DK.Framework.UWP.Test/Mocks/MockNavigationTarget.cs
@@ -0,0 +1,52 @@
+﻿using DK.Framework.Core.Interfaces;
+using System;
+using System.Composition;
+
+namespace DK.Framework.Store.WinPhone8.Tests.Mocks
+{
+    /// <summary>
+    /// Mock navigation target for tests.
+    /// </summary>
+    [Export(typeof(INavigationTarget))]
+    [Shared]
+    public class MockNavigationTarget : INavigationTarget
+    {
+        public void Show(IScreen screen)
+        {
+            Current = screen;
+        }
+
+        public void Show(IScreen screen, Action<IScreen> initAction)
+        {
+            initAction(screen);
+            Current = screen;
+        }
+
+        public IScreen Current { get; private set; }
+
+        public void GoHome()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GoBack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GoForward()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanGoBack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanGoForward()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Store/Tests/DK.Framework.UWP.Test/Mocks/MockScreen.cs
@@ -0,0 +1,48 @@
+﻿using DK.Framework.Core.Interfaces;
+using DK.Framework.UWP.Attributes;
+using DK.Framework.Store.WinPhone8.Tests.ViewModels;
+using System;
+using System.ComponentModel;
+using System.Composition;
+
+namespace DK.Framework.Store.WinPhone8.Tests.Mocks
+{
+    /// <summary>
+    /// Mock screen for tests.
+    /// </summary>
+    [Screen(typeof(IScreen<TestViewModel>))]
+    [Shared]
+    public class MockScreen : IScreen<TestViewModel>
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public TestViewModel ViewModel { get; set; }
+
+        public string Title { get; set; }
+
+        public MockScreen()
+        {
+            ViewModel = new TestViewModel();
+        }
+
+        public void Start(Action completed)
+        {
+            completed();
+        }
+
+        public void End(Action completed)
+        {
+            completed();
+        }
+
+        public Type ScreenType
+        {
+            get { return typeof(MockScreen); }
+        }
+
+        /// <summary>
+        /// The relative location of the screen.
+        /// </summary>
+        public string Location { get { return "Mock.xaml"; } }
+    }
+}
